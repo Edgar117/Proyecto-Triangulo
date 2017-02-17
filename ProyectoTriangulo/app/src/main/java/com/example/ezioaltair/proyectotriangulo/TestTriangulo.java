0aaf62cdc75365eab1209967a28523b92f1f891a
YennBastarrachea15 <yenibastarrachea15@gmail.com>
2017-02-17 07:51:49 -0600

    Documentacion de proyecto por medio de comentarios
    se comentaron los métodos, variables, operaciones que se realizan entre
    otras cosas

--- ProyectoTriangulo/app/src/main/java/com/example/ezioaltair/proyectotriangulo/TestTriangulo.java
+++ ProyectoTriangulo/app/src/main/java/com/example/ezioaltair/proyectotriangulo/TestTriangulo.java
@@ -11,33 +11,34 @@ public class TestTriangulo extends Triangulo {
     //Metodos set y get de los lados de los traingulos
     public double getA() {
         return a;
-    }
+    } //estos métodos sirven para poder obtener el valor A
 
     public void setA(double a) {
         this.a = a;
-    }
+    } //estos métodos sirven para poder enviar el valor A
 
     public double getB() {
         return b;
-    }
+    }//estos métodos sirven para poder obtener el valor B
 
     public void setB(double b) {
         this.b = b;
-    }
+    } //estos métodos sirven para poder enviar el valor B
 
     public double getC() {
         return c;
-    }
+    }//estos métodos sirven para poder obtener el valor C
 
     public void setC(double c) {
         this.c = c;
-    }
+    } //estos métodos sirven para poder enviar el valor B
 
     public TestTriangulo()
     {
 
     }
 
+    //Método para calcular el area del triángulo y luego lo devuelve
     public double area() {
         double s = (a+b+c)/2;
         //return Math.sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
--- ProyectoTriangulo/app/src/main/java/com/example/ezioaltair/proyectotriangulo/Triangulo.java
+++ ProyectoTriangulo/app/src/main/java/com/example/ezioaltair/proyectotriangulo/Triangulo.java
@@ -12,9 +12,9 @@ import android.widget.Toast;
 
 public class Triangulo extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editTextA, editTextB, editTextC;
-    private Button btnOperacion;
-    private TextView textView;
+    private EditText editTextA, editTextB, editTextC; //Variables donde se guardaran los datos ingresos
+    private Button btnOperacion; //Boton que servirá para que al presionar pueda hace el calculo
+    private TextView textView; //Para mostrar el resultad
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +24,26 @@ public class Triangulo extends AppCompatActivity implements View.OnClickListener
        /* ImageView logo = (ImageView)findViewById(R.id.iv1);
         logo.setImageResource(R.drawable.triangulo);*/
 
-        editTextA = (EditText) findViewById(R.id.editTextA);
-        editTextB = (EditText) findViewById(R.id.editTextB);
-        editTextC = (EditText) findViewById(R.id.editTextC);
-        btnOperacion = (Button) findViewById(R.id.btnOperacion);
-        textView = (TextView) findViewById(R.id.textViewR);
+        editTextA = (EditText) findViewById(R.id.editTextA); //Pasar los datos ingresados en los text a las variables creadas
+        editTextB = (EditText) findViewById(R.id.editTextB);  //Pasar los datos ingresados en los text a las variables creadas
+        editTextC = (EditText) findViewById(R.id.editTextC); //Pasar los datos ingresados en los text a las variables creadas
+        btnOperacion = (Button) findViewById(R.id.btnOperacion); //Instancia del boton creado
+        textView = (TextView) findViewById(R.id.textViewR); //Instancia del textView creado
 
         btnOperacion.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
+        // se obtienen las variables y se convierten a tipo double para hacer las operaciones
         double a = Double.parseDouble(editTextA.getText().toString());
         double b = Double.parseDouble(editTextB.getText().toString());
         double c = Double.parseDouble(editTextC.getText().toString());
 
+        //operación para hacer el calculo del área
         double suma = a+b+c;
         double Divisor=suma/2;
 
+        //variables para la verificacion de si es triángulo
         double Opercion1=Divisor-a;
         double Operacion2=Divisor-b;
         double Operacion3=Divisor-c;
@@ -48,39 +51,41 @@ public class Triangulo extends AppCompatActivity implements View.OnClickListener
 
 // Validación para verificar si es un triángulo
 
-        if (Opercion1 + Operacion2 > Operacion3){
-            if (Operacion2 + Operacion3 > Opercion1){
-                if (Opercion1 + Operacion3 > Operacion2){
-                    double Multiplicacion = Divisor*Opercion1*Operacion2*Operacion3;
-                    double raiz=Math.sqrt(Multiplicacion);
-                    textView.setText("Area = " + raiz);
-                    Toast toast1 = Toast.makeText(getApplicationContext(), "SI, es un triangulo", Toast.LENGTH_SHORT);
-                    toast1.show();
+        if (Opercion1 + Operacion2 > Operacion3){ // compara el lado  A y B para verificar si son mayores que C
+            if (Operacion2 + Operacion3 > Opercion1){ // compara si el lado B y C son mayores que el A
+                if (Opercion1 + Operacion3 > Operacion2){ // compara si el lado A y C son mayores que el B
+                    double Multiplicacion = Divisor*Opercion1*Operacion2*Operacion3; // se hace la operacion indicada
+                    double raiz=Math.sqrt(Multiplicacion); //se saca la raiz del producto obtenido de multiplicacion
+                    textView.setText("Area = " + raiz); //se muestra la raíz del triángulo
+                    Toast toast1 = Toast.makeText(getApplicationContext(), "SI, es un triángulo", Toast.LENGTH_SHORT);
+                    toast1.show(); //muestra mensaje si es triángulo
                 }
                 else{
-                    double Multiplicacion = Divisor*Opercion1*Operacion2*Operacion3;
-                    double raiz=Math.sqrt(Multiplicacion);
-                    textView.setText("Area = " + raiz);
+                    double Multiplicacion = Divisor*Opercion1*Operacion2*Operacion3; // se hace la operacion indicada
+                    double raiz=Math.sqrt(Multiplicacion); //se saca la raiz del producto obtenido de multiplicacion
+                    textView.setText("Area = " + raiz); //se muestra la raíz del triángulo
                     Toast toast1 = Toast.makeText(getApplicationContext(), "SI, es un triangulo", Toast.LENGTH_SHORT);
-                    toast1.show();
+                    toast1.show(); //muestra mensaje si es triángulo
                 }
             }
             else {
                 Toast toast1 = Toast.makeText(getApplicationContext(), "NO, es un triangulo", Toast.LENGTH_SHORT);
-                toast1.show();
+                toast1.show(); //Muestra mensaje de que no es triangulo
             }
         }
         else {
             Toast toast1 = Toast.makeText(getApplicationContext(), "NO, es un triangulo", Toast.LENGTH_SHORT);
-            toast1.show();
+            toast1.show(); //Muestra mensaje de que no es triangulo
         }
 
         limpiar();
     }
+
+    //Método para limpiar los text
     public void limpiar(){
-        editTextA.setText("");
-        editTextB.setText("");
-        editTextC.setText("");
+        editTextA.setText(""); //limpia text A
+        editTextB.setText(""); //limpia text B
+        editTextC.setText(""); //limpia text C
     }
 
 }
